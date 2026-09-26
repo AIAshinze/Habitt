809fc4132efa53de6e540f76056382081550ab70
AIAshinze <tonyifyashinze@gmail.com>
2026-09-26 22:36:26 +0100

    feat: Refactor country fetching logic to support pagination and improve data handling

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,7 @@
 class ApiConfig {
   static const countriesApiKey = String.fromEnvironment(
     'REST_COUNTRIES_API_KEY',
+    defaultValue: 'rc_live_d2af13a275bf4f209296e96c786ed11b',
   );
 
   static const countriesBaseUrl = 'https://api.restcountries.com/countries/v5';
--- lib/country_list.dart
+++ lib/country_list.dart
@@ -7,23 +7,40 @@ import 'package:http/http.dart' as http;
 import '../config/api_config.dart';
 
 Future<List<String>> fetchCountries() async {
-  final uri = Uri.parse(
-    '${ApiConfig.countriesBaseUrl}?response_fields=names.common',
-  );
+  final List<String> allCountries = [];
+  int offset = 0;
+  const limit = 100;
+  bool hasMore = true;
 
-  final response = await http.get(
-    uri,
-    headers: {'Authorization': 'Bearer ${ApiConfig.countriesApiKey}'},
-  );
+  while (hasMore) {
+    final uri = Uri.parse(
+      '${ApiConfig.countriesBaseUrl}?response_fields=names.common&limit=$limit&offset=$offset',
+    );
 
-  if (response.statusCode != 200) {
-    throw Exception('Failed to load countries: ${response.statusCode}');
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer ${ApiConfig.countriesApiKey}'},
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load countries: ${response.statusCode}');
+    }
+
+    final json = jsonDecode(response.body);
+    final countryList = json['data']?['objects'] as List? ?? [];
+
+    for (final country in countryList) {
+      final name = country['names']?['common'] as String?;
+      if (name != null && name.isNotEmpty) {
+        allCountries.add(name);
+      }
+    }
+
+    final meta = json['meta'];
+    hasMore = meta?['more'] == true;
+    offset += limit;
   }
 
-  final json = jsonDecode(response.body);
-  final countryList = json['data']['objects'] as List;
-
-  return countryList
-      .map((country) => country['names']['common'] as String)
-      .toList();
+  allCountries.sort();
+  return allCountries;
 }
